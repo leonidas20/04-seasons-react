8943d97c7165cfb52cc185a820c897179ab2381c
Leonidas Theodoridis <leonidas.theodoridis@techem.de>
2019-05-14 16:22:35 +0200

    Refactoring (without constructor)

--- src/index.js
+++ src/index.js
@@ -11,23 +11,18 @@ import ReactDOM from 'react-dom';
 
 // React says we have to define render
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        // THIS IS THE ONLY TIME we do direct assignment
-        // to this.state
-        this.state = { lat: null, errorMessage: '' };
 
+    state = { lat: null, errorMessage:'' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // we called setstate!!!
-                this.setState({ lat: position.coords.latitude});
-            },
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({ lat: position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
         );
     }
 
+
+    // React says we have to define render!!!
     render () {
 
         if (this.state.errorMessage && !this.state.lat) {
