739d10bd4c797d62aaba1101c43ed484e0b7c7ea
Leonidas Theodoridis <leonidas.theodoridis@techem.de>
2019-05-14 14:15:28 +0200

    Final Commit - State in React Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+    return <div>Latitude: </div>
+};*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// React says we have to define render
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to this.state
+        this.state = { lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // we called setstate!!!
+                this.setState({ lat: position.coords.latitude});
+            },
+            (err) => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+
+    render () {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading!</div>;
+
+    }
+}
+
+ReactDOM.render (
+    <App />, document.querySelector("#root")
+);
